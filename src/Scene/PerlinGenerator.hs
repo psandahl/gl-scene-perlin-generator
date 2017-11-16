f3cc00b260bdf6225f358736008b1a2d5196d161
Patrik Sandahl <patrik.sandahl@gmail.com>
2017-11-16 15:53:44 +0100

    Generating images.

--- src/Scene/PerlinGenerator.hs
+++ src/Scene/PerlinGenerator.hs
@@ -6,9 +6,29 @@
 -- Stability: experimental
 -- Portability: portable
 module Scene.PerlinGenerator
-    ( module Scene.PerlinGenerator.GeneratorContext
+    ( genImage
+    , module Scene.PerlinGenerator.GeneratorContext
     , module Scene.PerlinGenerator.GeneratorQuery
     ) where
 
+import           Codec.Picture
+import           Control.Lens                           ((^.))
+import           Data.ByteString.Lazy                   (ByteString)
+import           Flow                                   ((<|))
+import           Linear                                 (V3, _y)
+import           Scene.PerlinGenerator.Algo             (perlinValue)
 import           Scene.PerlinGenerator.GeneratorContext
 import           Scene.PerlinGenerator.GeneratorQuery
+
+genImage :: GeneratorContext -> GeneratorQuery -> ByteString
+genImage context query =
+    encodePng <|
+        generateImage (\x -> toColor . perlinValue context query x)
+                      (width query) (height query)
+
+-- | Generate a color from the vectors y value. The y value goes from everything
+-- from rgb to alpha.
+toColor :: RealFrac a => V3 a -> PixelRGBA8
+toColor vec =
+    let val = round <| vec ^. _y
+    in PixelRGBA8 val val val val

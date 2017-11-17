e909d0357edf8d3cb13f4056cf7bf9f052dc831e
Patrik Sandahl <patrik.sandahl@gmail.com>
2017-11-17 10:28:21 +0100

    Refactoring.

--- app/Main.hs
+++ app/Main.hs
@@ -3,25 +3,37 @@ module Main
     ( main
     ) where
 
+import           Data.Map.Strict                      (Map)
+import qualified Data.Map.Strict                      as Map
 import           Data.Text.Lazy                       (Text)
 import           Flow                                 ((<|))
 import           Network.HTTP.Types                   (badRequest400)
 import           Network.Wai.Middleware.RequestLogger (logStdoutDev)
+import           Scene.Math                           (Weight)
 import           Scene.PerlinGenerator
 import           Web.Scotty
 
+type WeightMap = Map Text [Weight]
+
 main :: IO ()
-main =
+main = do
+    let context = defaultGeneratorContext
+        weightMap = makeWeightMap
     scotty 8000 $ do
         middleware logStdoutDev
 
-        get "/image/default" <| genImage defaultGeneratorContext `rescue` badRequest
+        get "/image/:selection" <| genImage context weightMap `rescue` badRequest
 
-genImage :: GeneratorContext -> ActionM ()
-genImage context = do
-    query <- generatorQuery
-    setHeader "Content-Type" "image/png"
-    raw <| genSerializedImageRGB8 context query
+genImage :: GeneratorContext -> WeightMap -> ActionM ()
+genImage context weightMap = do
+    selection <- param "selection"
+    case Map.lookup selection weightMap of
+        Just ws -> do
+            query <- generatorQuery
+            setHeader "Content-Type" "image/png"
+            raw <| genSerializedImageRGB8 (context { weights = ws }) query
+
+        Nothing -> raise "Cannot find weight map"
 
 badRequest :: Text -> ActionM ()
 badRequest msg = do
@@ -35,3 +47,7 @@ generatorQuery =
                    <*> pure 1024
                    <*> pure 1024
                    <*> pure 255
+
+makeWeightMap :: WeightMap
+makeWeightMap =
+    Map.fromList [("singleton", singletonWeight)]

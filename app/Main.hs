505f75229eaa485eb4dad158ecee2842e860176e
Patrik Sandahl <patrik.sandahl@gmail.com>
2017-11-17 09:50:21 +0100

    Generate some sort of image. Seems buggy.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,37 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main
     ( main
     ) where
 
+import           Data.Text.Lazy                       (Text)
+import           Flow                                 ((<|))
+import           Network.HTTP.Types                   (badRequest400)
+import           Network.Wai.Middleware.RequestLogger (logStdoutDev)
+import           Scene.PerlinGenerator
+import           Web.Scotty
+
 main :: IO ()
-main = putStrLn "Hello"
+main =
+    scotty 8000 $ do
+        middleware logStdoutDev
+
+        get "/image/default" <| genImage defaultGeneratorContext `rescue` badRequest
+
+genImage :: GeneratorContext -> ActionM ()
+genImage context = do
+    query <- generatorQuery
+    setHeader "Content-Type" "image/png"
+    raw <| genSerializedImageRGBA8 context query
+
+badRequest :: Text -> ActionM ()
+badRequest msg = do
+    status badRequest400
+    text msg
+
+generatorQuery :: ActionM GeneratorQuery
+generatorQuery =
+    GeneratorQuery <$> param "xPos"
+                   <*> param "yPos"
+                   <*> pure 1024
+                   <*> pure 1024
+                   <*> pure 255
